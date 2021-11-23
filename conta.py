809b05909d800543fd707077be5c7194bd473718
Cley-gabriel <gabrielcley23@hotmail.com>
2021-11-23 20:05:32 -0300

    Criação das Classes Conta, ContaCorrente e ContaPoupança

---
+++ conta.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+
+class Conta(ABC):
+    def __init__(self, agencia, conta, saldo):
+        self.agencia = agencia
+        self.conta = conta
+        self.saldo = saldo
+    
+    def depositar(self, valor):
+        self.saldo += valor
+        print(f'Você depositou {valor}')
+        print(f'Seu saldo é de {self.saldo}')
+        self.detalhes()
+
+    def detalhes(self):
+        print(f'Agencia: {self.agencia} '
+              f'Conta: {self.conta} '
+              f'Saldo: R${self.saldo:.2f} ')
+
+    @abstractmethod
+    def sacar(self, valor):
+        pass
+    
+#Conta corrente com limite de saque.
+class ContaCorrente(Conta):
+    def __init__(self, agencia, conta, saldo, limite = 100):
+        super().__init__(agencia, conta, saldo)
+        self.limite = limite
+    
+    def sacar(self, valor):
+        if (self.saldo + self.limite) < valor:
+            print('Saldo insuficiente')
+            return
+
+        self.saldo -= valor
+        print(f'Você fez um saque de R${valor}')
+        self.detalhes()
+
+class ContaPoupanca(Conta):
+    def sacar(self, valor):
+        if self.saldo < valor:
+            print(f'Saldo insuficiente, você tentou sacar R${valor} e o seu saldo é de R${self.saldo}')
+            return
+            
+        self.saldo -= valor
+        print(f'Você fez um saque de R${valor:.2f}')
+        self.detalhes()
+        
